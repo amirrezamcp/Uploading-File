7fecbfb0806b206b6198af3690a0f71c3fc3d7a1
bigpotato-yt <138766025+bigpotato-yt@users.noreply.github.com>
2023-10-07 21:22:10 +0330

    build-project-structure

---
+++ bootstrap/contants.php
@@ -0,0 +1,5 @@
+<?php
+
+define('SITE_TITLE','Uploading File');
+define('BASE_URL','http://localhost/PHP-Expert-Basic/PHP-Basic-Project/upload/');
+define('BASE_PATH','????');
---
+++ bootstrap/init.php
@@ -0,0 +1,5 @@
+<?php
+
+include_once "contants.php";
+include_once "config.php";
+include_once "../libs/lib-upload.php";
